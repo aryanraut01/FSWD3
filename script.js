5ec735c5dfec1b3679cd4d180e3ac3de909598c3
aryanraut01 <aryanraut435@gmail.com>
2026-03-27 13:42:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,36 +40,36 @@ function botReply(msg) {
         return "I am a simple chatbot.";
 
     else if (msg.includes("bye"))
-        return "Goodbye! Have a nice day.";
+        return "Goodbye! Have a nice day";
 
-    else if (msg.includes("what you can do?"))
+    else if (msg.includes("what you can do"))
         return "I can help you in any task you want.";
 
-    else if (msg.includes("what is artificial intelligence(ai)?"))
+    else if (msg.includes("what is artificial intelligence"))
         return "Artificial Intelligence (AI) is a technology that allows machines to learn, think, and make decisions similar to humans.";
 
-    if (msg.includes("can you help me with my homework?"))
+    else if (msg.includes("can you help me with my homework?"))
         return "Of course! What subject do you need help with?";
 
-    else if (msg.includes("math."))
+    else if (msg.includes("math"))
         return "Great! What math problem are you working on?";
 
-    else if (msg.includes("tell me about Python."))
+    else if (msg.includes("tell me about Python"))
         return "Do you mean Python the programming language or the snake?";
 
-    else if (msg.includes("the programming language."))
+    else if (msg.includes("the programming language"))
         return "Python is a popular programming language used for web development, data science, automation, and AI.";
 
-    else if (msg.includes("can you suggest a movie?"))
+    else if (msg.includes("can you suggest a movie"))
         return "Sure! What kind of movies do you like — action, comedy, or sci-fi?";
 
-    else if (msg.includes("comedy."))
+    else if (msg.includes("comedy"))
         return "You might enjoy a light comedy like Hera Fheri and Dhamal. Would you like more suggestions?";
 
-    else if (msg.includes("thanks for helping me."))
+    else if (msg.includes("thanks for helping me"))
         return "You're welcome!  Is there anything else I can help you with?";
 
-    else if (msg.includes("no."))
+    else if (msg.includes("no"))
         return "alright! have a great day!";
 
     else
